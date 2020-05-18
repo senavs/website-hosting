740a9ead40d180887b6c53d7762e1e845ccf97d3
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-05-18 16:31:55 -0300

    backend  main   - add dashboard to home page   - add values to deashboard (QUARTO)

--- project/backend/main/views.py
+++ project/backend/main/views.py
@@ -1,6 +1,29 @@
 from flask import make_response, render_template, url_for, abort, redirect
 
+from ..quarto.models import QuartoTipoQuartView, QuartoDisponivelView, QuartoIndisponivelView
+from ...configuration import DATABASE_URL
 
-# create your views funcs/classes here
+
 def home():
-    return render_template('home.html')
+    # quantidade total de quartos
+    with QuartoTipoQuartView(DATABASE_URL) as db:
+        qt_quartos = db.select_all()
+        qt_quartos = len(qt_quartos)
+
+    # quantidade total de quartos dispoíveis
+    with QuartoDisponivelView(DATABASE_URL) as db:
+        qt_quartos_disp = db.select_all()
+        qt_quartos_disp = len(qt_quartos_disp)
+
+    # quantidade total de quartos indispoíveis
+    with QuartoIndisponivelView(DATABASE_URL) as db:
+        qt_quartos_indisp = db.select_all()
+        qt_quartos_indisp = len(qt_quartos_indisp)
+
+    context = {
+        'qt_quartos': qt_quartos,
+        'qt_quartos_disp': qt_quartos_disp,
+        'qt_quartos_indisp': qt_quartos_indisp
+    }
+    print(context)
+    return render_template('home.html', **context)
