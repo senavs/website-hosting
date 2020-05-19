db9a816de37be8a55c5e10cd83df434cc114d043
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-05-19 10:26:20 -0300

    backend  home   - add views to PESSOAS dashboard

--- project/backend/main/views.py
+++ project/backend/main/views.py
@@ -1,29 +1,39 @@
 from flask import make_response, render_template, url_for, abort, redirect
 
 from ..quarto.models import QuartoTipoQuartView, QuartoDisponivelView, QuartoIndisponivelView
+from ..funcionario.models import FuncionarioEnderecoView
+from ..hospede.models import HospedeEnderecoView
 from ...configuration import DATABASE_URL
 
 
 def home():
     # quantidade total de quartos
     with QuartoTipoQuartView(DATABASE_URL) as db:
-        qt_quartos = db.select_all()
-        qt_quartos = len(qt_quartos)
+        qt_quartos = len(db.select_all())
 
     # quantidade total de quartos dispoíveis
     with QuartoDisponivelView(DATABASE_URL) as db:
-        qt_quartos_disp = db.select_all()
-        qt_quartos_disp = len(qt_quartos_disp)
+        qt_quartos_disp = len(db.select_all())
 
     # quantidade total de quartos indispoíveis
     with QuartoIndisponivelView(DATABASE_URL) as db:
-        qt_quartos_indisp = db.select_all()
-        qt_quartos_indisp = len(qt_quartos_indisp)
+        qt_quartos_indisp = len(db.select_all())
+
+    # quantidade de hóspede
+    with HospedeEnderecoView(DATABASE_URL) as db:
+        qt_hospedes = len(db.select_all())
+
+    # quantidade de hóspede
+    with FuncionarioEnderecoView(DATABASE_URL) as db:
+        qt_funcionarios = len(db.select_all())
 
     context = {
         'qt_quartos': qt_quartos,
         'qt_quartos_disp': qt_quartos_disp,
-        'qt_quartos_indisp': qt_quartos_indisp
+        'qt_quartos_indisp': qt_quartos_indisp,
+        'qt_pessoas': qt_hospedes + qt_funcionarios,
+        'qt_hospedes': qt_hospedes,
+        'qt_funcionarios': qt_funcionarios
     }
 
     return render_template('home.html', **context)
