5d5047c48475c69d736d80f460f71d068eea295d
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-05-19 22:19:08 -0300

    backend  home   - add views to HOSPEDAGEM dashboard

--- project/backend/main/views.py
+++ project/backend/main/views.py
@@ -3,6 +3,7 @@ from flask import make_response, render_template, url_for, abort, redirect
 from ..quarto.models import QuartoTipoQuartView, QuartoDisponivelView, QuartoIndisponivelView
 from ..funcionario.models import FuncionarioEnderecoView
 from ..hospede.models import HospedeEnderecoView
+from ..hospedagem.models import HospedagemModel, HospedagemTotalAtivo, HospedagemTotalReceber, HospedagemTotalRecebido
 from ...configuration import DATABASE_URL
 
 
@@ -27,13 +28,33 @@ def home():
     with FuncionarioEnderecoView(DATABASE_URL) as db:
         qt_funcionarios = len(db.select_all())
 
+    # quantidade de hospedagem
+    with HospedagemModel(DATABASE_URL) as db:
+        qt_hospedagem = len(db.select_all())
+
+    # quantidade de hospedagem ativas
+    with HospedagemTotalAtivo(DATABASE_URL) as db:
+        qt_hospedagem_ativo = db.select_all()[0]['TOTAL_ATIVO']
+
+    # quantidade de hospedagem a receber
+    with HospedagemTotalReceber(DATABASE_URL) as db:
+        qt_hospedagem_receber = db.select_all()[0]['TOTAL_A_RECEBER']
+
+    # quantidade de hospedagem recebido
+    with HospedagemTotalRecebido(DATABASE_URL) as db:
+        qt_hospedagem_recebido = db.select_all()[0]['TOTAL_RECEBIDO']
+
     context = {
         'qt_quartos': qt_quartos,
         'qt_quartos_disp': qt_quartos_disp,
         'qt_quartos_indisp': qt_quartos_indisp,
         'qt_pessoas': qt_hospedes + qt_funcionarios,
         'qt_hospedes': qt_hospedes,
-        'qt_funcionarios': qt_funcionarios
+        'qt_funcionarios': qt_funcionarios,
+        'qt_hospedagem': qt_hospedagem,
+        'qt_hospedagem_ativo': qt_hospedagem_ativo,
+        'qt_hospedagem_receber': qt_hospedagem_receber,
+        'qt_hospedagem_recebido': qt_hospedagem_recebido
     }
 
     return render_template('home.html', **context)
